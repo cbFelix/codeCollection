87f30488b2f86f49a433c16baefd8ceaf3c556db
kewwL <onerun325@gmail.com>
2025-02-06 14:44:54 +0500

    Small changes Short description: Bubble sort has been removed from the `binarySearch()` function and moved to the `main()` function.
    Full description:
    1. ./src/binarySearch.cpp - function binarySearch() - deleted built-in bubble sort.
    2. ./main.cpp - function main() - added bubble sort.

--- algorithms/search/binarySearch/main.cpp
+++ algorithms/search/binarySearch/main.cpp
@@ -7,7 +7,22 @@ int main() {
         const int arrSize = 16;
         int arr[arrSize] = {-15, 24, 0, 1000, 2, -45, -77, 1, -212, 92, 52, -1, -63, 12, 4, 12};
 
-        int targetIdx = binarySearch(arr, arrSize,1000);
+        int* array = new int[arrSize];
+
+        //      bubble sort
+        for (int i = 0; i < arrSize; i++) {
+                array[i] = arr[i];
+        }
+
+        for(int i = 0; i < arrSize; i++) {
+                for(int k = 1; k < arrSize; k++) {
+                        if(array[k - 1] > array[k]) {
+                                swap(array[k - 1], array[k]);
+                        }
+                }
+        }
+
+        int targetIdx = binarySearch(array, arrSize,1000);
 
         cout << targetIdx << endl;
 
--- algorithms/search/binarySearch/src/binarySearch.cpp
+++ algorithms/search/binarySearch/src/binarySearch.cpp
@@ -4,30 +4,14 @@ using namespace std;
 #include "binarySearch.hpp"
 
 int binarySearch(int* array, const int arrSize, int target) {
-        int* arr = new int[arrSize];
-
-        //      bubble sort
-        for (int i = 0; i < arrSize; i++) {
-                arr[i] = array[i];
-        }
-
-        for(int i = 0; i < arrSize; i++) {
-                for(int k = 1; k < arrSize; k++) {
-                        if(arr[k - 1] > arr[k]) {
-                                swap(arr[k - 1], arr[k]);
-                        }
-                }
-        }
-
-        //      
         int firstIdx = 0;
         int lastIdx = arrSize - 1;
 
         while (firstIdx <= lastIdx) {
                 int middleIdx = firstIdx + (lastIdx - firstIdx) / 2;
-                if (arr[middleIdx] == target) {
+                if (array[middleIdx] == target) {
                         return middleIdx;
-                } else if (arr[middleIdx] < target) {
+                } else if (array[middleIdx] < target) {
                         firstIdx = middleIdx + 1;
                 } else {
                         lastIdx = middleIdx - 1;

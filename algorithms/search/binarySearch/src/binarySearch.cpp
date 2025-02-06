de831ac238f02d3a9d44cffb2bfbba7d64d8713f
kewwL <zxcat174@gmail.com>
2025-02-06 14:27:25 +0500

    small fix

--- algorithms/search/binarySearch/src/binarySearch.cpp
+++ algorithms/search/binarySearch/src/binarySearch.cpp
@@ -13,7 +13,6 @@ int binarySearch(int* array, const int arrSize, int target) {
 
         for(int i = 0; i < arrSize; i++) {
                 for(int k = 1; k < arrSize; k++) {
-                        // Simple bubble sort
                         if(arr[k - 1] > arr[k]) {
                                 swap(arr[k - 1], arr[k]);
                         }
